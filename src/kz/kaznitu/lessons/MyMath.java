0316e21512e5e539410e94a32429737aab3763a3
Rakhmanov <rahmanov-adilet@mail.ru>
2017-10-02 16:11:07 +0600

    Hello.java аздап өзгерттік

--- src/kz/kaznitu/lessons/MyMath.java
+++ src/kz/kaznitu/lessons/MyMath.java
@@ -17,9 +17,9 @@ public class MyMath {
             return b;
     }
     public static int min(int a, int b){
-        if (a >= b)
-            return b;
-        else
+        if (a <= b)
             return a;
+        else
+            return b;
     }
 }
