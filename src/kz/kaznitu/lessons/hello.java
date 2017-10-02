b90061ae0cdcdfa0de538ef1242a74b9829cc6ab
Rakhmanov <rahmanov-adilet@mail.ru>
2017-10-02 15:42:30 +0600

    hello java аздап өзгерді

--- src/kz/kaznitu/lessons/hello.java
+++ src/kz/kaznitu/lessons/hello.java
@@ -5,5 +5,13 @@ public class hello {
         System.out.println("Hello Adilet!");
         System.out.println("Бұл зертханалық жұмыстын мақсаты:");
         System.out.println("GitHup-пен жұмысжасап үйрену.");
+
+        int a = MyMath.kvadrat(5) ;
+        int b = MyMath.kub(5) ;
+
+        System.out.println("a = " + a) ;
+        System.out.println("b = " + b) ;
+
+        System.out.println(MyMath.max(8,9));
     }
 }
