16c9fc42c697570bca7bc51efcb2f87f69c4be59
Rakhmanov <rahmanov-adilet@mail.ru>
2017-10-02 13:30:09 +0600

    git commit version 01."Hello.java өзгеріс енгіземіз"

--- src/kz/kaznitu/lessons/hello.java
+++ src/kz/kaznitu/lessons/hello.java
@@ -2,6 +2,8 @@ package kz.kaznitu.lessons;
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello Adilet");
+        System.out.println("Hello Adilet!");
+        System.out.println("Бұл зертханалық жұмыстын мақсаты:");
+        System.out.println("GitHup-пен жұмысжасап үйрену.");
     }
 }
