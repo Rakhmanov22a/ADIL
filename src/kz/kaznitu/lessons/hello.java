434c13ada2309f4af90d3b1ef7320436638fe33b
Rakhmanov <rahmanov-adilet@mail.ru>
2017-10-02 16:50:22 +0600

    experiment1 бұтағында Hello.java  файылына тағы өзгерістер жасалды

--- src/kz/kaznitu/lessons/hello.java
+++ src/kz/kaznitu/lessons/hello.java
@@ -6,13 +6,8 @@ public class hello {
         System.out.println("Бұл зертханалық жұмыстын мақсаты:");
         System.out.println("GitHup-пен жұмысжасап үйрену.");
 
-        int a = MyMath.kvadrat(5) ;
-        int b = MyMath.kub(5) ;
-
-        System.out.println("a = " + a) ;
-        System.out.println("b = " + b) ;
-
-        System.out.println(MyMath.max3(7,9,12 ));
+       System.out.println("max =" + MyMath.max3(8,9,12));
+       System.out.println("min =" + MyMath.min3(12,8,15));
 
 
     }
